0643976a783f9248f165b7c86a69e22251cdedc3
El Pibe <marcos.mikuska.galvao@escola.pr.gov.br>
2026-09-16 14:56:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function avaliarResposta(){
     //Criar as constantes que preciso
     const nome = document.getElementById("nome").value;
     const resposta = document.getElementById("resposta").value;
-    const textoDesafio = document.querySelector("# resultado h3").innerText;
+    const textoDesafio = document.querySelector("#resultado h3").innerText;
 
 
     if (resposta.trim() === "") {
@@ -118,12 +118,12 @@ function avaliarResposta(){
 
 ${pontos >= 70
 ? "Parabéns, você conseguiu uma ótima pontuação, continue assim!"
-: "Contunue pesquisando e desenvolvendo novos projetos, você consegurá na próxima!"
+: "Contiunue pesquisando e desenvolvendo novos projetos, você conseguirá na próxima!"
 
 }
 
     <button onclick="location.reload()">
-        Enviar a resposta.
+        Novo desafio!
     </button>
    `
 }
